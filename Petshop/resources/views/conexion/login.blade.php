20eef0a2b5a90da92b629f78b5a66bf72355e81d
LucasPezzuti <lukiu@live.com>
2020-04-10 17:51:54 -0300

    front del login

--- Petshop/resources/views/conexion/login.blade.php
+++ Petshop/resources/views/conexion/login.blade.php
@@ -1,4 +1,28 @@
 @extends('conexion.master')
 @section('title','login')
 @section('content')
+<div class="box box_login shadow">
+   {{-- escapo codigo html usando htmlcollective. le paso dentro del array donde se hara la peticion, en este caso url=> /login
+    el null en email es el parametro por defecto, y en el array van sus atributos, por ejemplo la clase de bootstrap form control--}} 
+    {!! Form::open(['url' => '/login']) !!}
+    <label for="email">Email</label>
+    <div class="input-group">
+        <div class="input-group-prepend">
+            <div class="input-group-text"><i class="fas fa-envelope"></i>
+            </div>
+        </div>
+    {!! Form::email('email', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <label for="email" class="mtop16">Password</label>
+    <div class="input-group">
+        <div class="input-group-prepend">
+            <div class="input-group-text"><i class="fas fa-lock"></i>
+            </div>
+        </div>
+    {!! Form::password('password', ['class' => 'form-control']) !!}
+    </div>
+    {!! Form::submit('Ingresar', ['class' => 'btn btn-success mtop16']) !!}
+    {!! Form::close() !!}  
+</div>
 @stop
--- Petshop/resources/views/conexion/master.blade.php
+++ Petshop/resources/views/conexion/master.blade.php
@@ -5,8 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 {{--     con yield declaro una variable de titulo que voy a sobreescribir en cada vista.--}}    
  <title>PetShop - @yield('title')</title>
- <link rel="stylesheet" href="{{ url('/static/css/conexion.css') }}">
-
+ <link rel="stylesheet" href="{{ url('/static/css/conexion.css?v='.time()) }}">
+{{--  ?v= es una variable que paso para revisar que no guarde en cache 
+    el css, que siempre lo cargue y el time nos muestra la hora en que se ejecuta para estar seguro   --}}  
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/cf91392eed.js" crossorigin="anonymous"></script>
+  
 </head>
 <body>
     

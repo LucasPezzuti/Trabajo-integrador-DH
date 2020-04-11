22353e052ea01ad4778b18beafa72814d736e9fc
NazaMangia <you@example.com>
2020-04-11 20:45:22 -0300

    Front del panel de administrador, iconos, tooltip con jquery, trabajo sobre la vista de admin y vinculacion de la vista con la bd

--- Petshop/resources/views/admin/master.blade.php
+++ Petshop/resources/views/admin/master.blade.php
@@ -10,6 +10,17 @@
     <link rel="stylesheet" href="{{ url('/static/css/admin.css?v='.time()) }}">
     {{-- fuente roboto de google fonts --}}
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/cf91392eed.js" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    <script>
+        //necesita esto jquery para iniciar
+        $(document).ready(function()){
+            $('[data-toggle="tooltip"]').tooltip()
+        }
+    </script>
 
     <title>@yield('title')</title>
 </head>
--- Petshop/resources/views/admin/sidebar.blade.php
+++ Petshop/resources/views/admin/sidebar.blade.php
@@ -1,3 +1,35 @@
 <div class="sidebar shadow">
+    <div class="section-top">
+        <div class="logo">
+            <img src="{{ url('static/images/logo2.png') }}" class="img-fluid">
+        </div> 
+        
+        <div class="user">
+            <span class="subtitle">Hola:</span>
+
+            <div class="name">
+                {{ Auth::user()->nombre }} {{ Auth::user()->apellido }}
+            <a href="{{ url('/logout') }}" data-toggle="tooltip" data-placement="top" title="Salir"><i class="fas fa-sign-out-alt"></i></a>
+            </div>
+
+            <div class="email">{{ Auth::user()->email }}
+            </div>
+        </div>
+    </div>
     
+    <div class="main">
+        <ul>
+            <li>
+            <a href="{{ url('/admin') }}"><i class="fas fa-home"></i>Administrador</a>
+            </li>
+
+            <li>
+                <a href="{{ url('/admin/productos') }}"><i class="fas fa-boxes"></i>Productos</a>
+            </li>
+
+            <li>
+                <a href="{{ url('/admin/users') }}"><i class="fas fa-users"></i>Usuarios</a>
+            </li>
+        </ul>
+    </div>
 </div>

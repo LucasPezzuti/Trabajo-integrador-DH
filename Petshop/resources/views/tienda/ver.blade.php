e49bca303141dba96d1c3f0df76b44e68641a5c1
LucasPezzuti <lukiu@live.com>
2020-05-27 17:53:06 -0300

    correcciones

--- Petshop/resources/views/tienda/ver.blade.php
+++ Petshop/resources/views/tienda/ver.blade.php
@@ -23,7 +23,7 @@
 										<div class="product_image_1 product_image">
 
 											<a href="{{ url('/uploads/'.$producto[0]->file_path.'/'.$producto[0]->imagen) }}" data-fancybox="gallery"> {{-- le paso como link la misma imagen pero sin el prefijo t_ para q al darle click abra la imagen grande --}}
-                                                <img src="{{ url('/uploads/'.$producto[0]->file_path.'/t_'.$producto[0]->imagen) }}" width="700"  alt="product"></td> {{-- aca hago uso del campo file_path de la tabla donde se guarda el path de la imagen --}}{{--  t_ es para llamar a la miniatura --}}
+                                                <img src="{{ url('/uploads/'.$producto[0]->file_path.'/t_'.$producto[0]->imagen) }}" width="500"  alt="product"></td> {{-- aca hago uso del campo file_path de la tabla donde se guarda el path de la imagen --}}{{--  t_ es para llamar a la miniatura --}}
                                             </a>
 
 										</div>
@@ -34,36 +34,37 @@
 									
 								</div>
                             </div>
-                            
-							<div class="product_sidebar order-lg-2 order-1">
-								<form action="#" id="product_form" class="product_form">
-                                    <div class="product_name" style="padding-bottom:20px">{{ $producto[0]->nombre }}</div>
 
-                                    @php
-                                    if($producto[0]->endescuento == 0)
-                                    $producto[0]->endescuento = 'No';
-                                    else
-                                    $producto[0]->endescuento = 'Si';
-                                    @endphp
+									<div class="product_sidebar order-lg-2 order-1" style="-moz-box-sizing: border-box; -webkit-box-sizing: border-box; box-sizing: border-box; padding-right:800px;width: auto;margin-left:20px">
+										<form action="#" id="product_form" class="product_form">
+											<div class="product_name" style="padding-bottom:20px">{{ $producto[0]->nombre }}</div>
 
-                                    <div class="">¿En descuento?: {{ $producto[0]->endescuento }}</div>
-                                    <div class="">Descuento: {{ $producto[0]->descuento }}%</div>
-                                    <div class="product_price">${{ $producto[0]->precio }}</div>
-                                    <div class="">
+											@php
+											if($producto[0]->endescuento == 0)
+											$producto[0]->endescuento = 'No';
+											else
+											$producto[0]->endescuento = 'Si';
+											@endphp
 
-                                        Descripción:
+											<div class="">¿En descuento?: {{ $producto[0]->endescuento }}</div>
+											<div class="">Descuento: {{ $producto[0]->descuento }}%</div>
+											<div class="product_price">${{ $producto[0]->precio }}</div>
+											<div class="">
 
-                                        @php
-                                        $cont = $producto[0]->contenido;
-                                        echo strip_tags(html_entity_decode($cont));
-                                        @endphp
-                                        
+												Descripción:
+
+												@php
+												$cont = $producto[0]->contenido;
+												echo strip_tags(html_entity_decode($cont));
+												@endphp
+												
+											</div>
+											<div class="product_button ml-auto mr-auto trans_200"><a href="{{route('cart-add',$producto[0]->slug ) }}">añadir</a></div>
+											{{-- //<button class="cart_button trans_200" href="{{route('cart-add', $producto[0]->slug ) }}">Añadir al carrito</button> --}}
+										</form>
+										
 									</div>
-									<div class="product_button ml-auto mr-auto trans_200"><a href="{{route('cart-add',$producto[0]->slug ) }}">añadir</a></div>
-									{{-- //<button class="cart_button trans_200" href="{{route('cart-add', $producto[0]->slug ) }}">Añadir al carrito</button> --}}
-								</form>
-								
-                            </div>
+
                         
 						</div>
 					</div>
